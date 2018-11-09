f4912caf4ed6faf37a349802b364f37386b19260
Fritz Reitz <freitz@uw.edu>
2018-11-09 11:07:24 -0800

    tailoring ac/bal4charts, colors right, dashes right, excluded research grants (balances not calculating correct for those yet) and future dates, legend just re-ordering and not showing dashes or checkboxes

--- Research/AC/Bal4charts.aspx.cs
+++ Research/AC/Bal4charts.aspx.cs
@@ -34,7 +34,9 @@ public partial class AC_Bal4charts : BasePage
 	{
 		SQL_utils sql = new SQL_utils("FinMart");
 
-		DataTable dt_bnums = sql.DataTable_from_SQLstring("select budgetNumber,  [name] budgetName from fin.tblEDWData where bienniumYear = 2017 group by budgetNumber,  [name]");
+		DataTable dt_bnums = sql.DataTable_from_SQLstring("select budgetNumber,  " +
+            "[name] budgetName from fin.tblEDWData " +
+            "where bienniumYear = 2017 and budgetNumber in (095001, 095002, 099877, 653920, 655411, 640259, 653977, 656876, 658823, 680475) group by budgetNumber,  [name]");
 
 		if (dt_bnums.Columns[0].ColumnName == "err")
 		{
@@ -55,9 +57,16 @@ public partial class AC_Bal4charts : BasePage
 				string bname = row["budgetName"].ToString();
 				string bnum = row["budgetNumber"].ToString();
 
+                string mo = DateTime.Now.Month.ToString();
+                if (mo.Length < 2) { mo = "0" + mo; }
+                string yrmo_now = DateTime.Now.Year.ToString() + "_" + mo;
 
-				string code = String.Format("select '1' id, budgetNumber, [name] budgetName, bienniumYear, yrmo, RevenueAmount, REV_Endowment, REV_Other, ExpenseAmount, EXP_SALARY, EXP_Non_SALARY, balance from fin.tblEDWData where bienniumYear = 2017 and budgetNumber = '{0}'"
-					, bnum);
+                string code = String.Format(
+                    "select '1' id, budgetNumber, [name] budgetName, bienniumYear, yrmo, " +
+                    "RevenueAmount, REV_Endowment, REV_Other, -ExpenseAmount as ExpenseAmount, " +
+                    "-EXP_SALARY as EXP_SALARY, -EXP_Non_SALARY as EXP_Non_SALARY, balance " +
+                    "from fin.tblEDWData where bienniumYear = 2017 and budgetNumber = '{0}' and yrmo <= '{1}'"
+                    , bnum, yrmo_now);
 
 				DataTable dt = sql.DataTable_from_SQLstring(code);
 
@@ -68,16 +77,57 @@ public partial class AC_Bal4charts : BasePage
 				{
 					DxLineplotSettings settings = new DxLineplotSettings();
 
-					List<string> yvars = new List<string> { "RevenueAmount", "REV_Endowment", "REV_Other", "ExpenseAmount", "EXP_SALARY", "EXP_Non_SALARY", "balance" };
+					List<string> yvars = new List<string> { 
+                          "RevenueAmount"
+                        , "REV_Endowment"
+                        , "REV_Other"
+                        , "ExpenseAmount"
+                        , "EXP_SALARY"
+                        , "EXP_Non_SALARY"
+                        , "balance" 
+                    };
 
+					List<Color> mycolors = new List<Color> { 
+                         Color.DarkBlue //balance
+                        ,Color.Red //exp non-sal
+                        ,Color.Red //exp sal
+                        ,Color.Red //exp tot
+                        ,Color.Green //rev endow
+                        ,Color.Green //rev other
+						,Color.Green //rev tot
 
-					List<Color> mycolors = new List<Color> { Color.Lime, Color.Red, Color.Green, Color.Purple, Color.Yellow
-							, Color.Cyan , Color.Pink, Color.Peru, Color.Wheat, Color.SkyBlue
-							, Color.SpringGreen, Color.AliceBlue, Color.Orange, Color.DarkOrange, Color.Olive};
+                        // Color.Red //balance
+                        //,Color.Orange //exp non-sal
+                        //,Color.Yellow //exp sal
+                        //,Color.Green //exp tot
+                        //,Color.Blue //rev endow
+                        //,Color.Purple //rev other
+						//,Color.Cyan //rev tot
 
-					settings.maxCol = 2;
-					settings.W = 800;
-					settings.H = 600;
+                        //,Color.Pink
+                        //,Color.Peru
+                        //,Color.Wheat
+                        //,Color.SkyBlue
+						//,Color.SpringGreen
+                        //,Color.AliceBlue
+                        //,Color.Orange
+                        //,Color.DarkOrange
+                        //,Color.Olive
+                    };
+
+                    List<DashStyle> mydashstyles = new List<DashStyle> {
+                         DashStyle.Solid
+                        ,DashStyle.Dash
+                        ,DashStyle.Dot
+                        ,DashStyle.Solid
+                        ,DashStyle.Dot
+                        ,DashStyle.Dash
+                        ,DashStyle.Solid
+                    };
+
+                    settings.maxCol = 2;
+					settings.W = 1200;
+					settings.H = 800;
 					settings.colors = mycolors;
 					settings.numvars = yvars;
 					settings.seriesby = "id";
@@ -86,7 +136,7 @@ public partial class AC_Bal4charts : BasePage
 					settings.title = String.Format("{0} {1}", bnum, bname);
 					settings.legend_pos_h = "RightOutside";
 					settings.legend_pos_v = "Top";
-
+                    settings.ChartType = DxChartType.Lineplot;
 
 					DxChartOrder order = new DxChartOrder();
 					order.list_settings.Add(settings);
@@ -101,16 +151,33 @@ public partial class AC_Bal4charts : BasePage
 						{
 							WebChartControl c = batch.charts[0].chart;
 
-							DevExpress.XtraCharts.Series s0 = c.Series[0];
+                            c.Legend.MarkerMode = LegendMarkerMode.CheckBoxAndMarker;
+
+                            DevExpress.XtraCharts.Series s0 = c.Series[0];
 							DevExpress.XtraCharts.Series s1 = c.Series[1];
+                            DevExpress.XtraCharts.Series s2 = c.Series[2];
+                            DevExpress.XtraCharts.Series s3 = c.Series[3];
+                            DevExpress.XtraCharts.Series s4 = c.Series[4];
+                            DevExpress.XtraCharts.Series s5 = c.Series[5];
+                            DevExpress.XtraCharts.Series s6 = c.Series[6];
 
-							((LineSeriesView)s0.View).LineStyle.DashStyle = DashStyle.DashDotDot;
-							((LineSeriesView)s1.View).LineStyle.DashStyle = DashStyle.Dash;
+                            s0.View.Color = mycolors[0];
+                            s1.View.Color = mycolors[1];
+                            s2.View.Color = mycolors[2];
+                            s3.View.Color = mycolors[3];
+                            s4.View.Color = mycolors[4];
+                            s5.View.Color = mycolors[5];
+                            s6.View.Color = mycolors[6];
 
+                            ((LineSeriesView)s0.View).LineStyle.DashStyle = mydashstyles[0];
+							((LineSeriesView)s1.View).LineStyle.DashStyle = mydashstyles[1];
+                            ((LineSeriesView)s2.View).LineStyle.DashStyle = mydashstyles[2];
+                            ((LineSeriesView)s3.View).LineStyle.DashStyle = mydashstyles[3];
+                            ((LineSeriesView)s4.View).LineStyle.DashStyle = mydashstyles[4];
+                            ((LineSeriesView)s5.View).LineStyle.DashStyle = mydashstyles[5];
+                            ((LineSeriesView)s6.View).LineStyle.DashStyle = mydashstyles[6];
 
-
-
-							System.Web.UI.WebControls.Table t = ChartOutput.LayoutBatch(batch);
+                            System.Web.UI.WebControls.Table t = ChartOutput.LayoutBatch(batch);
 							Literal lit = new Literal();
 							lit.Text = String.Format("<b>{0} {1}</b>", bnum, bname);
 							panel.Controls.Add(lit);
