ca8cd70bebe6e135b946f44ac09107a2ce8ff7c9
Fritz Reitz <freitz@uw.edu>
2020-02-28 16:50:15 -0800

    got test_get_BI to return something, albeit a UW login page

--- Research/AC/test_get_BI.aspx.cs
+++ Research/AC/test_get_BI.aspx.cs
@@ -10,24 +10,30 @@ public partial class test_get_BI : BasePage //System.Web.UI.Page
     }
     public void Read_Month()
     {
-        String urlString = "https://edw.washington.edu/ReportServer/Pages/ReportViewer.aspx?"
-            + "/ Financial / Variable Reporting Period Budget Summary"
-            + "& rs:Command = Render"
-            + "& rs:format = csv"
-            + "& startdate = nov 2019"
-            + "& enddate = dec 2019"
-            + "& myfdbudlist = N / A"
-            + "& encumbrances = True"
-            + "& expiredsalary = True"
-            + "& inckids = n"
-            + "& budgetnbr = 65 - 3920"
-            + "& reportby = s";
+        //String urlString = "https://edw.washington.edu/ReportServer/Pages/ReportViewer.aspx?"
+        //    + "/Financial/Variable Reporting Period Budget Summary"
+        //    + "&rs:Command=Render"
+        //    + "&rs:format=csv"
+        //    + "&startdate=nov 2019"
+        //    + "&enddate=dec 2019"
+        //    + "&myfdbudlist=N/A"
+        //    + "&encumbrances=True"
+        //    + "&expiredsalary=True"
+        //    + "&inckids=n"
+        //    + "&budgetnbr=65-3920"
+        //    + "&reportby=s";
+
+
+        String urlString = "https://ucs.admin.uw.edu/MyFD/UWNetID/BudgetSummary.aspx?"
+            + "FdiNavbar:ViewBudgetNumberTxt=653920"
+            + "&FdiNavbar:TimePeriodList=Feb"
+            + "&FdiNavbar:CalendarYearList=2020";
 
         WebRequest request = WebRequest.Create(urlString);
 
-        request.Credentials = BuildCredentials(urlString, "netid\freitz", "Fr1tz007", "Bearer");
+        request.Credentials = BuildCredentials(urlString, @"netid\freitz", "Fr1tz007", "Windows");
 
-        WebResponse response = request.GetResponse();
+        WebResponse response = request.GetResponse(); // response is netid sign in page HTML!
 
         Console.WriteLine(((HttpWebResponse)response).StatusDescription);
 
@@ -44,12 +50,23 @@ public partial class test_get_BI : BasePage //System.Web.UI.Page
 
     private ICredentials BuildCredentials(string siteurl, string username, string password, string authtype)
     {
-        NetworkCredential cred = new System.Net.NetworkCredential(username, password);
-
+        NetworkCredential cred;
+        if (username.Contains(@"\"))
+        {
+            string domain = username.Substring(0, username.IndexOf(@"\"));
+            username = username.Substring(username.IndexOf(@"\") + 1);
+            cred = new System.Net.NetworkCredential(username, password, domain);
+        }
+        else
+        {
+            cred = new System.Net.NetworkCredential(username, password);
+        }
         CredentialCache cache = new CredentialCache();
-
+        if (authtype.Contains(":"))
+        {
+            authtype = authtype.Substring(authtype.IndexOf(":") + 1); //remove the TMG: prefix
+        }
         cache.Add(new Uri(siteurl), authtype, cred);
-
         return cache;
     }
 }
